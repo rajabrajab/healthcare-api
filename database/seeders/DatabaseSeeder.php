cff11c84d62d7ec6f90b274c3fdcfe607198f807
rajabrajabb <141490342+rajabrajab@users.noreply.github.com>
2025-05-27 15:27:12 +0300

    fixing the apis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(FoodSeeder::class);
         $this->call(LifeStyleSeeder::class);
         $this->call(FoodSeeder::class);
+
+        $this->call(LifeStyleSeeder::class);
+        $this->call(FoodLogSeeder::class);
     }
 }
