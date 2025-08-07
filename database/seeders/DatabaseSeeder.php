90a373dba26ecf396fe21fcf837687c83723854b
rajabrajabb <141490342+rajabrajab@users.noreply.github.com>
2025-08-07 12:40:32 +0300

    un seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,12 +9,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminSeeder::class);
-        $this->call(ReadingLogSeeder::class);
+        // $this->call(ReadingLogSeeder::class);
 
         $this->call(FoodSeeder::class);
         $this->call(LifeStyleSeeder::class);
 
-        $this->call(LifestyleLogSeeder::class);
-        $this->call(FoodLogSeeder::class);
+        // $this->call(LifestyleLogSeeder::class);
+        // $this->call(FoodLogSeeder::class);
     }
 }
